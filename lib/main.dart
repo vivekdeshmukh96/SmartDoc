4a1b475869d56585c3a05881a9098ee54d15ea2a
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-08 05:30:39 +0000

    Integrate Firebase into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:collegeapplication/firebase_options.dart';
 import 'package:collegeapplication/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const SmartDocApp());
 }
 
