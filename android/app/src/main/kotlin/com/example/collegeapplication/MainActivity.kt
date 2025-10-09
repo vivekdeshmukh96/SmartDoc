a0891e40e5b4dbfa1ed8c7755c97daa8a265d417
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-09 20:18:08 +0000

    Revert manual plugin registration

--- android/app/src/main/kotlin/com/example/collegeapplication/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/collegeapplication/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.example.collegeapplication
 
 import io.flutter.embedding.android.FlutterActivity
-import vn.hunghd.flutter.plugins.imagecropper.ImageCropperPlugin
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        flutterEngine.plugins.add(ImageCropperPlugin())
-    }
-}
+class MainActivity: FlutterActivity()
