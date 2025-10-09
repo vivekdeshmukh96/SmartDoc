64ddf2cbbeb4ada815c943a158e38a85f94e551c
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-09 20:16:06 +0000

    Fix Android build errors

--- android/app/src/main/kotlin/com/example/collegeapplication/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/collegeapplication/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.collegeapplication
 
 import io.flutter.embedding.android.FlutterActivity
+import vn.hunghd.flutter.plugins.imagecropper.ImageCropperPlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(ImageCropperPlugin())
+    }
+}
