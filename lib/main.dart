60fd705a29665d35a0a1f98a3f9e5692c7df63a3
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-10 07:25:14 +0000

    Fix Firebase initialization error

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
+import 'firebase_options.dart';
 import 'screens/common/auth_wrapper.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
