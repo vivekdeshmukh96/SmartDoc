68c62429d7ea07e416338b6c5cb5a78741f1352b
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-10 07:04:38 +0000

    Implement login flow

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const ScannerScreen(), // Temporarily set to ScannerScreen for development
+        home: const AuthScreen(),
       ),
     );
   }
