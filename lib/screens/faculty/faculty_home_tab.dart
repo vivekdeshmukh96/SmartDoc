47ad040f07e5e3847001fdcb7d06f34857d18aea
deshmukhvivek596 <deshmukhvivek596@gmail.com>
2025-10-15 18:36:53 +0000

    Show student list on faculty home page

--- lib/screens/faculty/faculty_home_tab.dart
+++ lib/screens/faculty/faculty_home_tab.dart
@@ -1,10 +1,50 @@
-import 'package:flutter/material.dart';
 
-class FacultyHomeTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:smart_doc/models/user.dart';
+
+class FacultyHomeTab extends StatefulWidget {
   const FacultyHomeTab({super.key});
 
   @override
+  _FacultyHomeTabState createState() => _FacultyHomeTabState();
+}
+
+class _FacultyHomeTabState extends State<FacultyHomeTab> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home Tab'));
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .where('role', isEqualTo: 'student')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No students found.'));
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            final doc = snapshot.data!.docs[index];
+            final user = User.fromFirestore(doc.data() as Map<String, dynamic>, doc.id);
+
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(user.photoURL ?? ''),
+              ),
+              title: Text(user.name ?? 'No Name'),
+              subtitle: Text(user.className ?? 'No Class'),
+              onTap: () {
+                // TODO: Navigate to student documents screen
+              },
+            );
+          },
+        );
+      },
+    );
   }
 }
